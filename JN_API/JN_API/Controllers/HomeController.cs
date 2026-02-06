a4709c0c0172f861dc00b148999a08340be60986
Eduardo Calvo Castillo <ecalvo@crnova.com>
2026-02-05 20:39:56 -0600

    Método post del api para crear usuarios

--- JN_API/JN_API/Controllers/HomeController.cs
+++ JN_API/JN_API/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
+using Dapper;
 using JN_API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
 
 namespace JN_API.Controllers
 {
@@ -10,11 +13,16 @@ namespace JN_API.Controllers
         [HttpPost(Name = "RegistroUsuario")]
         public IActionResult RegistroUsuario(Usuario model)
         {
-            /* 
-               Reglas de negocio
-               Cálculos
-               Llamadas a la base de datos
-            */
+            using (var context = new SqlConnection("Server=localhost\\MSSQLSERVER01;Database=JN_DB;Integrated Security=True;TrustServerCertificate=True;"))
+            {
+                var parametros = new DynamicParameters();
+                parametros.Add("@Identificacion", model.Identificacion);
+                parametros.Add("@Nombre", model.Nombre);
+                parametros.Add("@CorreoElectronico", model.CorreoElectronico);
+                parametros.Add("@Contrasenna", model.Contrasenna);
+
+                context.Execute("sp_RegistrarCuenta", parametros);
+            }
 
             return Ok();
         }
