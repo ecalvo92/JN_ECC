80e474f30ed6a166372f69b0972b88df455ed43e
Eduardo Calvo Castillo <ecalvo@crnova.com>
2026-02-12 20:47:49 -0600

    Ajuste de estilos y jquery validate

---
+++ JN_WEB/JN_WEB/wwwroot/js/Login.js
@@ -0,0 +1,32 @@
+﻿$(function () {
+
+  $("#formLogin").validate({
+    rules: {
+      CorreoElectronico: {
+        required: true,
+        email: true
+      },
+      Contrasenna: {
+        required: true
+      }
+    },
+    messages: {
+      CorreoElectronico: {
+        required: "Campo obligatorio",
+        email: "Formato incorrecto"
+      },
+      Contrasenna: {
+        required: "Campo obligatorio",
+      }
+    },
+    errorElement: "span",
+    errorClass: "text-danger",
+    highlight: function (element) {
+      $(element).addClass("is-invalid");
+    },
+    unhighlight: function (element) {
+      $(element).removeClass("is-invalid");
+    }
+  });
+
+});
