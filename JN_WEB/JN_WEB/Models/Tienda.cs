c2819e5e93bf0391806b6162f6e08df404d54609
Eduardo Calvo Castillo <ecalvo@crnova.com>
2026-03-26 19:19:43 -0600

    Diseño y validaciones de la vista de tienda

---
+++ JN_WEB/JN_WEB/Models/Tienda.cs
@@ -0,0 +1,10 @@
+﻿namespace JN_WEB.Models
+{
+    public class Tienda
+    {
+        public string Nombre { get; set; } = string.Empty;
+        public string Contacto { get; set; } = string.Empty;
+        public string Descripcion { get; set; } = string.Empty;
+        public string Ubicacion { get; set; } = string.Empty;
+    }
+}
---
+++ JN_WEB/JN_WEB/wwwroot/js/CambiarTienda.js
@@ -0,0 +1,56 @@
+﻿$.validator.addMethod("telefono", function (value, element) {
+  return this.optional(element) || /^\d{8}$/.test(value);
+}, "Debe contener exactamente 8 dígitos numéricos");
+
+$(function () {
+
+  $("#formCambiarTienda").validate({
+    rules: {
+      Nombre: {
+        required: true,
+        maxlength: 50
+      },
+      Descripcion: {
+        required: true,
+        maxlength: 500
+      },
+      Contacto: {
+        required: true,
+        telefono: true,
+        maxlength: 50
+      },
+      Ubicacion: {
+        required: true,
+        maxlength: 50
+      }
+    },
+    messages: {
+      Nombre: {
+        required: "Campo obligatorio",
+        maxlength: "Máximo 50 caracteres"
+      },
+      Descripcion: {
+        required: "Campo obligatorio",
+        maxlength: "Máximo 500 caracteres"
+      },
+      Contacto: {
+        required: "Campo obligatorio",
+        telefono: "Formato de número telefónico inválido",
+        maxlength: "Máximo 50 caracteres"
+      },
+      Ubicacion: {
+        required: "Campo obligatorio",
+        maxlength: "Máximo 50 caracteres"
+      }
+    },
+    errorClass: "text-white",
+    errorElement: "span",
+    highlight: function (element) {
+      $(element).addClass("is-invalid");
+    },
+    unhighlight: function (element) {
+      $(element).removeClass("is-invalid");
+    }
+  });
+
+});
